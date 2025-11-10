49aa59f36abc63fd44c2fc18fadbb2ec931c0db1
nkuthalomakonco <nkuthalomakonco@gmail.com>
2025-11-10 11:25:59 +0200

    https://www.youtube.com/watch?v=s1bk-68aB1U

---
+++ RMDataManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RMDataManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
